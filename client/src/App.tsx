ca85c7a7c540d23cd16b7b7b7aa2bb4dc687cecb
Kerasiya Vishnu <vishnuk@ziguts.com>
2023-06-09 13:23:58 +0100

    routing and navbar setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import { useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "@/views/navbar/index";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline /> Hii
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<>Dashborad </>} />
+              <Route path="/predictions" element={<>predictions </>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
